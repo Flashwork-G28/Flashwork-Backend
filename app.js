9f4466c62b71a55354b931bea40b2b9d18dc4cfa
WRLAKSHAN <wrlakshan@gmail.com>
2022-08-22 23:21:21 +0530

    create job provider

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
 const express = require('express');
-const router_student = require('./routes/student');
-const errorHandler = require('./utils/errorHandler');
+const router_auth0 = require('./routes/auth0');
+// const errorHandler = require('./utils/errorHandler');
+
 const app = express();
 app.use(express.json( ));
 const PORT = 8000;
+let cors = require('cors');
 
-app.use(errorHandler);
+app.use(cors())
 
+// app.use(errorHandler);
+
+
+
+app.use("/register",router_auth0);
 
 // not need
 
-app.all('/', (req, res) => {
-    console.log(req.body);
-
-    // res.send({
-    //     data: "hii"
-    // }) //send data
-
-    res.status(200).json({
-        data: "hi with status"
-    })//send with status
-})
+// app.all('/', (req, res) => {
+//     console.log(req.body);
+//     // res.send({
+//     //     data: "hii"
+//     // }) //send data
+//     res.status(200).json({
+//         data: "hi with status"
+//     })//send with status
+// })
 
 ////////////////////////////////
 
-app.use("/student",router_student);
+// app.use("/student",router_student);
 
 // app.all("*", (req, res, next) => {
 //     next(new AppError(`url ${req.originalUrl} not found`, 404));
