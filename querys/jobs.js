9077eeab2447ccd0db7cea307d061ef95c83b976
WRLAKSHAN <wrlakshan@gmail.com>
2022-09-08 11:07:09 +0530

    create all by user 1.1

--- querys/jobs.js
+++ querys/jobs.js
@@ -3,3 +3,5 @@ exports.GET_JOBS = 'SELECT * FROM jobs,user WHERE jobs.user_id = user.user_id;'
 exports.GET_LATESTJOBS = 'SELECT * FROM jobs,user WHERE jobs.user_id = user.user_id ORDER BY jobs.id DESC LIMIT 1'
 
 exports.APPLY_FOR_JOB = 'INSERT INTO job_apply ( id, job_id, seeker_id, date) VALUES (NULL,?, ?,CURRENT_TIMESTAMP);'
+
+exports.UPDATE_AMC = 'UPDATE jobs SET amc = amc - 1 WHERE id = ?'
