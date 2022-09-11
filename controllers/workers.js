7488abeba282ec9ff31c2576a7e12e4ce825b2e1
RangikaShalani <rangika2520@gmail.com>
2022-09-11 14:29:02 +0530

    get worker card all details 1.1

--- controllers/workers.js
+++ controllers/workers.js
@@ -1,23 +1,39 @@
 const conn = require("../services/db");
 const {GET_WORKERS} = require("../querys/workers");
+const {GET_WORKERS_RATE} = require("../querys/workers");
+
 
 exports.getWorkers = async (request, response ) => {
-    // console.log("data");
-
+    let userdata = [];
     conn.query(GET_WORKERS, (err, data, fields) => {
         if(err) {
             // console.log(err);
             response.status(401).json({
                 data: err
             })
-        }
-        else {
-            // console.log(workers);
-            response.status(200).send({
-                data}
+        } else {
+            // console.log(data);
+            data.map(value => {
+                // console.log(value.user_id)
+                conn.query(GET_WORKERS_RATE,[value.user_id,'1',value.user_id,value.user_id], (err, data_worker, fields) => {
+                    // console.log("tewst");
+                    if(err) {
+                        // console.log(err);
+                        response.status(401).json({
+                            data_worker: err
+                        })
+                    } else {
+                        // let rate= data1.AVG(rate);
+                        console.log(data_worker);
+                        console.log("hi");
+
+                    }
+                })
+            })
+
+            response.status(200).send(
+                data
             )
-
-
         }
     })
 }
--- querys/workers.js
+++ querys/workers.js
@@ -1,6 +1,3 @@
+exports.GET_WORKERS = 'SELECT user.user_id, user.first_name , user.last_name , job_seeker.description , job_seeker.category FROM user INNER JOIN job_seeker ON user.user_id = job_seeker.user_id;'
 
-
-
-exports.GET_WORKERS = 'SELECT user.first_name,user.last_name,job_seeker.category,job_seeker.description FROM job_seeker,user WHERE user.user_id=job_seeker.user_id;'
-
-// She sells sea shells on the sea shore
+exports.GET_WORKERS_RATE = 'SELECT AVG(rate) AS rating , COUNT(rate_receiver_id= ?) AS rating_count ,(SELECT COUNT(status) FROM complet_job WHERE status=?  AND job_seeker_id= ?) AS complet_count FROM rate WHERE rate_receiver_id= ?;'
