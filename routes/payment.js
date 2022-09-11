42b384fcbfded014a1d5b4f91abfb882d3c853d2
WRLAKSHAN <wrlakshan@gmail.com>
2022-09-11 22:50:32 +0530

    finish pay

--- routes/payment.js
+++ routes/payment.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const {yearPayment, monthPayment} = require("../services/payment");
+const {yearPayment, monthPayment,pro} = require("../services/payment");
 const router = express.Router();
 
-router.route("/year")
+router.route("/year/:id/:price")
     .post(yearPayment)
 
-router.route("/month")
+router.route("/month/:id/:price")
     .post(monthPayment)
+
+router.route("/pro")
+    .post(pro)
 
 module.exports = router;
--- services/payment.js
+++ services/payment.js
@@ -1,10 +1,12 @@
 const conn = require("./db");
+const {INSET_PAY, PRO} = require("../querys/payment");
+
 
 const stripe = require('stripe')('sk_test_51Lg6dfHV4pEFJsd7Sd0SPxg658tAKFbl2Tys5kJx6kdIKNODZTx1D8dqbfpIjHctBdXGMqobLrRg9rinqiR7wv2y00lkiTvZXT');
 const YOUR_DOMAIN = 'http://localhost:3000';
 
 exports.yearPayment = async (req, res) => {
-    console.log("hello");
+    console.log(req.params);
     const session = await stripe.checkout.sessions.create({
         line_items: [
             {
@@ -19,12 +21,30 @@ exports.yearPayment = async (req, res) => {
         cancel_url: `${YOUR_DOMAIN}?canceled=true`,
     });
 
+    const d = new Date();
+    const year = d.getFullYear();
+    const month = d.getMonth();
+    const day = d.getDate();
+    const end = new Date(year + 1, month, day);
+
+    conn.query(INSET_PAY,[req.params.id,req.params.price,end], (err, data, fields) => {
+        if(err) {
+            // console.log(data);
+            response.status(401).json({
+                data: err
+            })
+        } else {
+            console.log(data);
+
+        }
+    })
     res.redirect(303, session.url);
 };
 
 
+
 exports.monthPayment = async (req, res) => {
-    console.log(req);
+    console.log(req.params);
     const session = await stripe.checkout.sessions.create({
         line_items: [
             {
@@ -37,6 +57,41 @@ exports.monthPayment = async (req, res) => {
         success_url: `${YOUR_DOMAIN}?success=true`,
         cancel_url: `${YOUR_DOMAIN}?canceled=true`,
     });
+    const d = new Date();
+    const year = d.getFullYear();
+    const month = d.getMonth();
+    const day = d.getDate();
+    const end = new Date(year , month +1, day);
+
+    conn.query(INSET_PAY,[req.params.id,req.params.price,end], (err, data, fields) => {
+        if(err) {
+            // console.log(data);
+            response.status(401).json({
+                data: err
+            })
+        } else {
+            console.log(data);
+
+        }
+    })
 
     res.redirect(303, session.url);
 };
+
+
+exports.pro = async (request, response ) => {
+    // console.log(request.body);
+    conn.query(PRO,[request.body.id], (err, data, fields) => {
+        if(err) {
+            // console.log(err);
+            response.status(401).json({
+                data: err
+            })
+        } else {
+            // console.log(data);
+            response.status(200).send(
+                data
+            )
+        }
+    })
+}
