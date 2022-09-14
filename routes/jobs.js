c9e0e5a0cbc7c97df0bf2782d98db9bb787b0daa
udesh98 <um4me.98@gmail.com>
2022-09-14 11:09:29 +0530

    Job advertisement backend updated

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {getJobs, getLatestJobs, applyForJob, JobAdPost} = require('../controllers/jobs')
+const {getJobs, getLatestJobs, applyForJob, JobAdPost, JobAdView, DeleteJobs} = require('../controllers/jobs')
 
 const router = express.Router();
 
@@ -13,6 +13,12 @@ router.route("/apply")
     .post(applyForJob)
 
 router.route("/JobAdPost")
-    .post(JobAdPost) 
+    .post(JobAdPost)
+
+router.route("/JobAdView")
+    .get(JobAdView)
+
+router.route("/DeleteJobs")
+    .delete(DeleteJobs)
 
 module.exports = router;
