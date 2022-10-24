44f4d197d2fb53e5e8e9f3dac02c1fa027b3c1a9
udesh98 <um4me.98@gmail.com>
2022-10-24 15:05:51 +0530

    Admin can view complaints

--- controllers/complaints.js
+++ controllers/complaints.js
@@ -1,15 +1,32 @@
 const conn = require("../services/db");
-const {JOB_SEEKER_COMPL} = require("../querys/complaints");
+const {JOB_SEEKER_COMPL, VIEW_COMPL} = require("../querys/complaints");
 
 exports.JobSeekerCompl = async (request, response) => {
     console.log(request.body);
     conn.query(JOB_SEEKER_COMPL, [request.body.email, request.body.reason, request.body.rating], function(err, data, fields) {
         if(err) {
-            console.log(err);
+            // console.log(err);
             response.status(401).json('Sorry!! Commplaint is not posted')
         } else {
-            console.log(data);
+            // console.log(data);
             response.status(201).json('Complaint successful!')
+        }
+    })
+}
+
+exports.ViewCompl = async (request, response) => {
+    console.log(request.body);
+    conn.query(VIEW_COMPL, function(err, data, fields) {
+        if(err) {
+            // console.log(err);
+            response.status(401).json({
+                data: err
+            })
+        } else {
+            // console.log(data);
+            response.status(200).send(
+                data
+            )
         }
     })
 }
