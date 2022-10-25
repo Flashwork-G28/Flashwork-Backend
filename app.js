b0f7cd13043e974a7f31874f070f795cc2d46d3f
RangikaShalani <rangika2520@gmail.com>
2022-10-25 22:17:10 +0530

    Update notification

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const router_auth0 = require('./routes/auth0');
 const router_jobs = require('./routes/jobs');
 const router_workers = require('./routes/workers');
 const router_provider = require('./routes/jobProvider');
+
 // const errorHandler = require('./utils/errorHandler');
 
 const app = express();
@@ -19,6 +20,7 @@ app.use("/register",router_auth0);
 app.use("/jobs",router_jobs);
 app.use("/workers",router_workers);
 app.use("/jobProvider",router_provider);
+
 
 
 
--- controllers/jobProvider.js
+++ controllers/jobProvider.js
@@ -1,8 +1,8 @@
 const conn = require("../services/db");
-const {GET_BOOKING_WORKERS,GET_SAVED_WORKERS} = require("../querys/jobProvider");
+const {GET_BOOKING_WORKERS,GET_SAVED_WORKERS,GET_NOTIFICATION,GET_NOTIFICATION_COUNT} = require("../querys/jobProvider");
 
 exports.getBookingWorkers = async (request, response ) => {
-    console.log(request.body);
+    // console.log(request.body);
 
     conn.query(GET_BOOKING_WORKERS,[request.body.job_provider_id], (err, data, fields) => {
 
@@ -11,7 +11,7 @@ exports.getBookingWorkers = async (request, response ) => {
                 data: err
             })
         } else {
-            console.log(data);
+            // console.log(data);
             response.status(200).send(
                 data
             );
@@ -21,20 +21,61 @@ exports.getBookingWorkers = async (request, response ) => {
 }
 
 exports.getSavedWorkers = async (request, response ) => {
-    console.log(request.body);
-
     conn.query(GET_SAVED_WORKERS,[request.body.job_provider_id], (err, data, fields) => {
+        if(err) {
+            response.status(401).json({
+                data: err
+            })
+        } else {
+            response.status(200).send(
+                data
+            );
+        }
+    })
+}
+
+exports.getNotification = async (request, response ) => {
+
+    conn.query(GET_NOTIFICATION,[request.body.job_provider_id], (err, data, fields) => {
 
         if(err) {
             response.status(401).json({
                 data: err
             })
         } else {
-            console.log(data);
             response.status(200).send(
                 data
             );
         }
-
     })
+
 }
+
+exports.getNotificationCount = async (request, response ) => {
+
+    conn.query(GET_NOTIFICATION_COUNT,[request.body.job_provider_id], (err, data, fields) => {
+
+        if(err) {
+            response.status(401).json({
+                data: err
+            })
+        } else {
+            response.status(200).send(
+                data
+            );
+        }
+    })
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
